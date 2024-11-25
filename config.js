60ce564c5d2e93c04e7c89679b58927d261f297b
Euluna Gotami <eulunag@gmail.com>
2024-11-25 03:41:59 -0800

    Automatically randomize plant of the day based on current date

--- config.js
+++ config.js
@@ -1,4 +1,21 @@
 const config = {
     API_KEY: 'sk-Or7J67444db32db537761',
-    PLANT_OF_DAY_ID: 1
+    PLANT_OF_DAY_ID: (() => {
+        // Get current date in YYYYMMDD format
+        const today = new Date();
+        const dateString = today.getFullYear().toString() + 
+                          (today.getMonth() + 1).toString().padStart(2, '0') + 
+                          today.getDate().toString().padStart(2, '0');
+        
+        // Convert date string to a number and use it as a seed
+        let seed = parseInt(dateString);
+        
+        // Use a larger multiplier to get better distribution
+        seed = seed * 1234567;
+        
+        // Use modulo to get a number between 0 and 10101, then add 1
+        const randomNum = (seed % 10102) + 1;
+        
+        return randomNum;
+    })()
 };
