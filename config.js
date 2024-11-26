36dc44dceb119e58120685e80b204efb5b2cb478
Euluna Gotami <131542211+euluna@users.noreply.github.com>
2024-11-25 16:33:21 -0800

    Update key

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-    API_KEY: 'sk-l48m674515d4917ee7317',
+    API_KEY: 'sk-FtXT6745171be0b297774',
     PLANT_OF_DAY_ID: (() => {
         // Get current date in YYYYMMDD format
         const today = new Date();
